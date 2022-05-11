3bb9dab0204bd8f23bb5a74f94a2d34a50661ecd
antoni-codes <emmanuel_general@hotmail.com>
2022-05-10 22:56:25 -0500

    fix: cambiando ruta de json test de Reader

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,10 +1,10 @@
-const Reader = require('./../../lib/utils/Reader')
+const Reader = require('../../lib/utils/Reader');
 
 describe('Pruebas de Utils Reader', () => {
     test('1. Test de respuesta método readJsonFile', () => {
-        const docJson = 'app/explorers.json';
+        const docJson = 'app/Reader_test.json';
         const explorers = Reader.readJsonFile(docJson);
 
-        expect(explorers).toBe('app/explorers.json')
+        expect(explorers.length).toBe(1);
     })
 })
