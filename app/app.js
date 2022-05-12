cd239313de87ebbfb436eb9a7e5abeac60e5ca3f
antoni-codes <emmanuel_general@hotmail.com>
2022-05-11 19:24:24 -0500

    fix: moviendo archivos a sus respectivos directorios

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ const explorers = JSON.parse(rawdata);
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
 // console.log(explorersInNode.length)
-
+ 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
 const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
