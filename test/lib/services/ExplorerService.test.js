4defbbd344720c603b32076bdc3968f3f05aa815
antoni-codes <emmanuel_general@hotmail.com>
2022-05-11 20:10:48 -0500

    test: agregando test 2 Para obtener cantidad total de exploradores

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -1,11 +1,21 @@
 const ExplorerService = require('./../../../lib/services/ExplorerService')
+const ExplorersJson = require('./../../../explorers.json')
 
 describe("Pruebas de ExplorerService", () => {
-    test("Test de filtrado por misión", () => {
-        const explorer = [{ mission: 'node' }]
+    test("1. Test de filtrado por misión", () => {
+        const explorer = ExplorersJson
         const mission = "node"
         const explorersInNode = ExplorerService.filterByMission(explorer, mission);
 
-        expect(explorersInNode.length).toBe(1)
+        expect(explorersInNode.length).toBe(10)
+    })
+
+    test("2. Test obtener cantidad de exploradores", () => {
+        const explorer = ExplorersJson;
+        const mission = "node";
+        const explorersTotal = ExplorerService.getAmountOfExplorersByMission(explorer, mission);
+
+        expect(explorersTotal.length).toBe(10)
+
     })
 })
