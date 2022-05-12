628f0d1c9d0e01c403b4b95ad0cabccdbcfb22e1
antoni-codes <emmanuel_general@hotmail.com>
2022-05-11 19:23:25 -0500

    test: agregando código que filtra por misión node un json

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -1,1 +1,11 @@
+const ExplorerService = require('./../../../lib/services/ExplorerService')
 
+describe("Pruebas de ExplorerService", () => {
+    test("Test de filtrado por misión", () => {
+        const explorer = [{ mission: 'node' }]
+        const mission = "node"
+        const explorersInNode = ExplorerService.filterByMisssion(explorer, mission);
+
+        expect(explorersInNode.length).toBe(1)
+    })
+})
