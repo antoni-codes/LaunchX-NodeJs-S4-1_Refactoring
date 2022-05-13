1e74eef2e12daf88ffee329309b23d6550166e37
antoni-codes <emmanuel_general@hotmail.com>
2022-05-13 02:39:45 -0500

    fix: archivos eliminaods

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -1,10 +1,10 @@
-const Reader = require('../../lib/utils/Reader');
+const Reader = require('./../../../lib/utils/Reader');
 
 describe('Pruebas de Utils Reader', () => {
     test('1. Test de respuesta método readJsonFile', () => {
-        const docJson = './../../../explorers.json';
+        const docJson = "explorers.json";
         const explorers = Reader.readJsonFile(docJson);
 
-        expect(explorers.length).toBe(1);
+        expect(explorers.length).toBe(15);
     })
 })
