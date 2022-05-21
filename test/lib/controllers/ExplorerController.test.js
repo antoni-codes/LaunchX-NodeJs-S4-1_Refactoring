39659b28a3423df9bdcfa919bc30ed3dac391038
antoni-codes <emmanuel_general@hotmail.com>
2022-05-20 21:33:30 -0500

    test: 1. test para lectura de JSON

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -1,1 +1,12 @@
+const ExplorerService = require('../../../lib/services/ExplorerService')
+const FizzbuzzService = require('../../../lib/services/FizzbuzzService')
+const Reader = require('../../../lib//utils/Reader')
 
+describe('Testing para Explorer Controllers', () => {
+    test('1. Test para probar lectura de JSON', () => {
+        const listJson = 'explorers.json'
+        const readingJson = Reader.readJsonFile(listJson)
+
+        expect(readingJson.length).toBe(15)
+    })
+})
