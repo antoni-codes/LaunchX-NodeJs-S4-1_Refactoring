428396ea1827717f2f10d6600492c4548cc39a3e
antoni-codes <emmanuel_general@hotmail.com>
2022-05-20 23:09:33 -0500

    test: agregando 2. test para obtener lista de explorers por misión

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -1,12 +1,19 @@
 const ExplorerService = require('../../../lib/services/ExplorerService')
 const FizzbuzzService = require('../../../lib/services/FizzbuzzService')
 const Reader = require('../../../lib//utils/Reader')
+const ExplorerController = require('../../../lib/controllers/ExplorerController')
 
 describe('Testing para Explorer Controllers', () => {
-    test('1. Test para probar lectura de JSON', () => {
+    test('1. Test para probar lectura de archivo JSON', () => {
         const listJson = 'explorers.json'
         const readingJson = Reader.readJsonFile(listJson)
 
         expect(readingJson.length).toBe(15)
+    })
+
+    test('2. Test para obtener lista de Explorers por misión', () => {
+        const explorerByMission = ExplorerController.getExplorersByMission('node')
+        console.log(explorerByMission);
+        expect(explorerByMission.length).toBe(10)
     })
 })
